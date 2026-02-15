0548fc856780289f115988fe7675f4a0c8f1870b
unknown <sgkhenat94@gmail.com>
2026-02-15 17:46:42 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dev.scanflowpro',
+  appName: 'ScanFlowPro',
+  webDir: 'www'
+};
+
+export default config;
